55fb09f577063ba27df51d3f86a2c59fd36385e6
Aleksandr <s_a_s_h_a_5@mail.ru>
2020-08-10 02:13:28 +0300

    Выгрузка сайта

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+
+	$('#show-modal-1').on('click', function (e) {
+		e.preventDefault();
+		$('#product-top__modal-1').show();
+
+	});
+
+	$('#close-modal-1').on('click', function (e) {
+		e.preventDefault();
+		$('#product-top__modal-1').hide();
+	});
+
+	$('#show-modal-2').on('click', function (e) {
+		e.preventDefault();
+		$('#product-top__modal-2').show();
+
+	});
+
+	$('#close-modal-2').on('click', function (e) {
+		e.preventDefault();
+		$('#product-top__modal-2').hide();
+	});
+
+	$('#show-modal-3').on('click', function (e) {
+		e.preventDefault();
+		$('#product-top__modal-3').show();
+
+	});
+
+	$('#close-modal-3').on('click', function (e) {
+		e.preventDefault();
+		$('#product-top__modal-3').hide();
+	});
+
+	$('#show-modal-4').on('click', function (e) {
+		e.preventDefault();
+		$('#product-top__modal-4').show();
+
+	});
+
+	$('#close-modal-4').on('click', function (e) {
+		e.preventDefault();
+		$('#product-top__modal-4').hide();
+	});
+
+});
